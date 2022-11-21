dbbd5615906e2786b5f94d49439ad3b5d3899350
ErsanSinani <ErsanSinani@users.noreply.github.com>
2022-11-21 15:23:09 +0100

    Begynt på livenyheter api. Henter api fra RAPIDAPI

--- GhostProjectv2/ClientApp/src/components/LiveNyheter.js
+++ GhostProjectv2/ClientApp/src/components/LiveNyheter.js
@@ -1,9 +1,32 @@
 ﻿import React, { Component } from 'react';
-import {  } from 'reactstrap';
+import { } from 'reactstrap';
+import $ from 'jquery';
 
+//Kode hentet fra https://rapidapi.com/deep.anuraj10/api/live-stock-market-news/
+
+//Koden brukes til å vise Akjse nyheter
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://live-stock-market-news.p.rapidapi.com/news/economic_times",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "2e0f850e20msh9b8ede82b348ad3p164211jsn9bc92debbda4",
+		"X-RapidAPI-Host": "live-stock-market-news.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
 
 export class LiveNyheter extends Component {
     static displayName = LiveNyheter.name;
 
+	render() {
+		<div></div>
 
+
+    }
 }
