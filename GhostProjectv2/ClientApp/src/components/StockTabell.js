fb8fda0b19573ae84f59e12f8bf6ec288587db01
kumia01 <thien11hei@gmail.com>
2022-11-22 11:28:20 +0100

    bytta api til yahoo finance

--- GhostProjectv2/ClientApp/src/components/StockTabell.js
+++ GhostProjectv2/ClientApp/src/components/StockTabell.js
@@ -5,12 +5,13 @@ import { data } from 'jquery';
 
 const options = {
 	method: 'GET',
-	url: 'https://latest-stock-price.p.rapidapi.com/any',
+	url: 'https://yh-finance.p.rapidapi.com/market/get-trending-tickers',
+	params: {region: 'US'},
 	headers: {
-		'X-RapidAPI-Key': 'fd945d0c24msh3ed33568bd9280fp10086djsn4f53ccc85b53',
-		'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
+	  'X-RapidAPI-Key': 'fd945d0c24msh3ed33568bd9280fp10086djsn4f53ccc85b53',
+	  'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
 	}
-};
+  };
 
 
 
@@ -28,8 +29,8 @@ export class StockTabell extends Component {
 	callAPI(){
 		axios.request(options)
 		.then((response) => {
-			console.log(response.data)
-			this.setState({list: response.data})
+			console.log(response.data.finance.result[0].quotes)
+			this.setState({list: response.data.finance.result[0].quotes})
 		})
 		.catch(function (error) {
 			console.error(error);
@@ -39,16 +40,17 @@ export class StockTabell extends Component {
     render() {
 		let data = this.state.list.slice(0,10).map((i,key) =>{
 			return(
-				<tr key="key">
-					<td>{i.symbol}</td>
-					<td>{i.open}</td>
-					<td>{i.change}</td>
+				<tr key={key}>
+					<th>{key+1}</th>
+					<td>{i.shortName}</td>
+					<td>{i.regularMarketPrice}</td>
+					<td>{i.regularMarketChange}</td>
 				</tr>
 			)
 		})
 		return (
 			<Table>
-				<thead><tr><th>aksje</th><th>pris</th><th>endring</th></tr></thead>
+				<thead><tr><th>#</th><th>aksje</th><th>pris</th><th>endring</th></tr></thead>
 				<tbody>
 					{data}
 				</tbody>
