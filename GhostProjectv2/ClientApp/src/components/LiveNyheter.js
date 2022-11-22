5d122a25f2252dac57d818773885fce45351ba89
ErsanSinani <ErsanSinani@users.noreply.github.com>
2022-11-22 12:59:01 +0100

    Endret til axios og endret api. Begynner på nyheter

--- GhostProjectv2/ClientApp/src/components/LiveNyheter.js
+++ GhostProjectv2/ClientApp/src/components/LiveNyheter.js
@@ -1,25 +1,30 @@
 ﻿import React, { Component } from 'react';
 import { } from 'reactstrap';
-import $ from 'jquery';
+import Axios from 'axios'
+
 
 //Kode hentet fra https://rapidapi.com/deep.anuraj10/api/live-stock-market-news/
 
 //Koden brukes til å vise Akjse nyheter
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://live-stock-market-news.p.rapidapi.com/news/economic_times",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "2e0f850e20msh9b8ede82b348ad3p164211jsn9bc92debbda4",
-		"X-RapidAPI-Host": "live-stock-market-news.p.rapidapi.com"
+const axios = require("axios");
+
+const options = {
+	method: 'GET',
+	url: 'https://live-stock-market-news.p.rapidapi.com/news/economic_times',
+	headers: {
+		'X-RapidAPI-Key': '2e0f850e20msh9b8ede82b348ad3p164211jsn9bc92debbda4',
+		'X-RapidAPI-Host': 'live-stock-market-news.p.rapidapi.com'
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
 });
+
+
 
 export class LiveNyheter extends Component {
     static displayName = LiveNyheter.name;
