9d41b4e06f7be1b0a9012b6262918b2d05e5cac6
StormVMSC <s351936@oslomet.no>
2022-11-24 14:59:43 +0100

    Kommentarer

--- GhostProjectv2/ClientApp/src/components/Historikk.js
+++ GhostProjectv2/ClientApp/src/components/Historikk.js
@@ -1,62 +1,98 @@
-﻿import React, { Component } from 'react';
+﻿{/* Imports */ }
+import React, { Component } from 'react';
+
+{/* Henter nødvendig funksjonalitet fra reactstrap */ }
 import { Container, Button, ButtonGroup, ButtonToolbar, Row, Col, Table} from 'reactstrap';
 
+{/* Js klassen Handel arver fra superklassen Component */ }
 export class Historikk extends Component {
-  static displayName = Historikk.name;
 
-  render () {
-    return (
-      <div>
-        <p>Overføringer</p>
-        <Container fluid="true">
-            <Row fluid="true">
-              <ButtonToolbar>
-                <ButtonGroup className="mr-2">
-                    <Button outline>Overfør</Button>
-                    <Button color="primary" sm="1">Transaksjoner</Button>
-                </ButtonGroup>
-            </ButtonToolbar>  
-            </Row>
-            <Row fluid="true">
-                <Col md="6" pt="3">
-                    <Table borderless>
-                        <thead>
-                            <tr>
-                            <th>Dato</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>11.02.2021</td>
-                            </tr>
-                            <tr>
-                                <td><i className="bi bi-arrow-left"> Overføring til konto: NOK 2, 0000</i></td>
-                            </tr>
-                            <tr>
-                                <td>03.01.2021</td>
-                            </tr>
-                            <tr>
-                                <td><i className="bi bi-arrow-right"> Innskudd: NOK 1, 000</i></td>
-                            </tr>
-                            <tr>
-                                <td>05.01.2021</td>
-                            </tr>
-                            <tr>
-                                <td><i className="bi bi-arrow-left"> Overføring av aksjer til bruker @ </i></td>
-                            </tr>
-                            <tr>
-                                <td>15.12.2020</td>
-                            </tr>
-                            <tr>
-                                <td><i className="bi bi-arrow-right"> Mottok aksjer fra bruker @</i></td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Col>
-            </Row>
-        </Container>
+    // Setter displayName til Historikk for eventuelle debugging meldinger
+    static displayName = Historikk.name;
 
-      </div>
-    );
-  }
-}
+    // Funksjon som kontrollerer container noden du står i
+    render() {
+
+        // Returnerer html elementene slik at de skrives ut
+        return (
+
+            // Div som inneholder html elementene til siden
+            <div>
+
+                {/* Tekst element */}
+                <p>Overføringer</p>
+
+                {/* Container som inneholder html elementer */}
+                <Container fluid="true">
+
+                    {/* Rad som skalerer på enhet */}
+                    <Row fluid="true">
+
+                        {/* Bruker ButtonToolbar for å holde knappe på rekke */}
+                        <ButtonToolbar>
+
+                            {/* ButtonGroup sørger for at knappene er samme størrelse */}
+                            <ButtonGroup className="mr-2">
+
+                                {/* Knapper, en av de bruker outline og den andre tar primary som farge */}
+                                <Button outline>Overfør</Button>
+                                <Button color="primary" sm="1">Transaksjoner</Button>
+                            </ButtonGroup>
+                        </ButtonToolbar>  
+                    </Row>
+
+                    {/* Rad som skalerer på enhet */}
+                    <Row fluid="true">
+
+                        {/* Kolonne som skal ta halve raden */}
+                        <Col md="6" pt="3">
+
+                            {/* Tabell som skal holde på overføringer */}
+                            <Table borderless>
+
+                                {/* Table Head, raden med informasjon om hva som står i kolonnene */}
+                                <thead>
+                                    {/* Markerer starten på en rad i tabellen */}
+                                    <tr>
+                                        {/* Table Head element */}
+                                        <th>Dato</th>
+                                    </tr>
+                                </thead>
+
+                                {/* Table Body, radene som skal inne holde informasjon */}
+                                <tbody>
+                                    {/* Table row, markerer start på en rad */}
+                                    <tr>
+                                        {/* Element i raden */}
+                                        <td>11.02.2021</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i className="bi bi-arrow-left"> Overføring til konto: NOK 2, 0000</i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>03.01.2021</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i className="bi bi-arrow-right"> Innskudd: NOK 1, 000</i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>05.01.2021</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i className="bi bi-arrow-left"> Overføring av aksjer til bruker @ </i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>15.12.2020</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i className="bi bi-arrow-right"> Mottok aksjer fra bruker @</i></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+      }
+    }
--- GhostProjectv2/ClientApp/src/components/Om.js
+++ GhostProjectv2/ClientApp/src/components/Om.js
@@ -1,23 +1,50 @@
-﻿import React, { Component } from 'react';
+﻿{/* Imports */}
+import React, { Component } from 'react';
+
+{/* Henter et bilde og gir det en id */}
 import oslomet from '../img/oslomet.jpg';
+
+{/* Henter funksjonaliteten for link fra react-router-dom */}
 import { Link } from 'react-router-dom';
+
+{/* Henter nødvendig funksjonalitet fra reactstrap */ }
 import { Button, Form, FormGroup, Label, Container, Col, Row } from 'reactstrap';
 
-
+{/* Js klassen Handel arver fra superklassen Component */ }
 export class Om extends Component {
+
+    // Setter displayName til Historikk for eventuelle debugging meldinger
     static displayName = Om.name;
 
+    // Funksjon som kontrollerer container noden du står i
     render() {
+
+        // Returnerer html elementene slik at de skrives ut
         return (
+
+            // Container som inneholder html elementene til siden
             <Container>
+
+                {/* Rad som skalerer på enhet */}
                 <Row fluid="true" className="align-items-center justify-content-center">
+
+                    {/* Kolonne som skal ta 100% av container vidde */}
                     <Col sm="12" md="6" lg="6">
+                        {/* Overskrift, bruker html elementet strong for å få fet skrift */}
                         <h2 className="text-center"><strong>Om Oss</strong></h2>
                     </Col>
                 </Row>
+
+                {/* Rad som skalerer på enhet */}
                 <Row fluid="true" className="align-items-center justify-content-center">
+
+                    {/* Kolonne som bestå av tekst, i denne raden skal det være to kolonner */}
+                    {/* Men, når det er en mobil eller en mindre enhet skal det bare være en kolonne */}
                     <Col md="6">
+                        {/* Undertittel */}
                         <h5 className="text-center"><strong>Ghost Finance</strong></h5>
+
+                        {/* Tekst */}
                         <p className="text-left">
                             Ghost Finance er en brukervennlig aksjehandel- og investeringsplattform.
                             Vårt mål er å gi både nybegynnere og mer erfarne aksjehandlere en høyfunksjonell og enkel plattform basert på det nordiske aksjemarkedet.
@@ -38,20 +65,31 @@ export class Om extends Component {
                             Storm Villenfeldt Viken - s351936 <br />
                         </p>
                     </Col>
+
+                    {/* Kolonne som skal bestå av bilde og informasjon om firma. Denne blir som en ny rad på mindre enheter */}
                     <Col fluid="true" md="6">
-                        <img src={oslomet} width="450"/>
+                        {/* Henter bildet fra id gitt i import */}
+                        <img src={oslomet} width="450" />
+
+                        {/* Tekst */}
                         <p>
                             Adresse: Pilestredet 35, 0166 Oslo <br />
                             Telefon: +47 46 53 47 44
                         </p>
                     </Col>
                 </Row>
+
+                {/* Rad som skal innehold link til kundeservice */}
                 <Row fluid="true" className="align-items-center justify-content-center">
+                    {/* Kolonne som tar halve siden, link skal stå under tekst elementene fra forrige rad */}
                     <Col md="6">
+                        {/* Bruker FormGroup for å kunne lage hyperlink til en komponent */}
                         <FormGroup>
                             <Link className="link-primary" to="/kundeservice">Til kundeservice</Link>
                         </FormGroup>
                     </Col>
+
+                    {/* Tom kolonne siden det er ønskelig at linken ligger under teksten og ikke på midten */}
                     <Col md="6"></Col>
                 </Row>
             </Container>
