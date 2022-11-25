52a13cefb5bb3d09d830eb414e770ea0d2b9dc12
kumia01 <thien11hei@gmail.com>
2022-11-25 17:51:45 +0100

    litt kode flytting på stocktabell

--- GhostProjectv2/ClientApp/src/components/StockTabell.js
+++ GhostProjectv2/ClientApp/src/components/StockTabell.js
@@ -13,6 +13,18 @@ const options = {
   };
 
 
+
+
+axios.request(options)
+	.then((response) => {
+		console.log(response.data)
+	})
+	.catch(function (error) {
+		console.error(error);
+	});
+
+
+
 export class StockTabell extends Component {
     static displayName = StockTabell.name;
 	constructor(props){
@@ -37,7 +49,6 @@ export class StockTabell extends Component {
 	callAskjeListe(){
 		axios.get('../Aksje/HentAlle')
 		.then((response) => {
-			console.log(response.data)
 			this.setState({
 				list: response.data
 			})
@@ -45,49 +56,9 @@ export class StockTabell extends Component {
 		.catch(function (error) {
 			console.error(error);
 		});
-		console.log(this.state.list)
 	}
 
-	/*callAPI(){
-		axios.request(options)
-		.then((response) => {
-			console.log(response.data)
-			response.data.map((i,key) =>{
-				const Aksje ={
-						ticker: i.symbol,
-						selskap: i.identifier,
-						pris: i.open,
-						gammelpris: i.lastPrice
-				}
-				this.state.list[key] = Aksje;
-			})
-			const innAksjer = this.state.list
-			console.log(innAksjer)
-			$.ajax({
-				url: "../Aksje/Lagre",
-				type: "POST",
-				data: JSON.stringify(innAksjer),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json",
-				success: function (OK) {
-					if (OK) {
-						//Sender Aksjer til 
-
-						console.log("FUCK YEAH!!");
-					}
-					else {
-						//Fikse error melding
-						document.getElementById("feil").textContent = "Feil i db - prøv igjen senere!";
-						console.log("FEIL!!");
-					}
-				}
-			})
-		})
-		.catch(function (error) {
-			console.error(error);
-		});
-
-	}*/
+	
 	
 	buy(ticker){
 
