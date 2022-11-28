0aad35c134db3e560014db34d454c5765813872e
ErsanSinani <72137313+ErsanSinani@users.noreply.github.com>
2022-11-28 23:52:05 +0100

    La til kundeservice. Ikke ferdig!

---
+++ xUnit_EnhetstestAvAkjseSide/KunderserviceTest.cs
@@ -0,0 +1,39 @@
+using GhostProjectv2.Controllers;
+using GhostProjectv2.DAL;
+using GhostProjectv2.Models;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnit_EnhetstestAvAkjseSide
+{
+    public class KunderserviceTest
+    {
+        [Fact]
+        public async Task LagreAsync()
+        {
+            //Arrange   
+            var innMelding = new Melding
+            {
+               Id = 1,
+               Navn = "Arnold",
+               Epost = "gettothechoppa@NOW.no",
+               Tekst = "æ Elsker stonks"
+
+            };
+
+            var mock = new Mock<IKundeserviceRepository>();
+            mock.Setup(ks => ks.Lagre(innMelding)).ReturnsAsync(true);
+            var kundeserviceController = new KundeserviceController(mock.Object);
+
+
+            //Act
+            bool result = await kundeserviceController.Lagre(innMelding);
+
+            //Assert
+            Assert.True(result);
+
+
+        }
+    }
+}
