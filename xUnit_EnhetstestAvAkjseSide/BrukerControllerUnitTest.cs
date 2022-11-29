64d9c9146e2b9ea8e59d2b8ce2ad4bf10474af52
Spermulf <gjermundhertzberg@gmail.com>
2022-11-29 13:10:24 +0100

    Fikset unit

---
+++ xUnit_EnhetstestAvAkjseSide/BrukerControllerUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using GhostProjectv2.Controllers;
+using GhostProjectv2.DAL;
+using GhostProjectv2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace xUnit_EnhetstestAvAkjseSide
+{
+    public class BrukerControllerUnitTest
+    {
+        private const string _loggetInn = "loggetInn";
+        private const string _ikkeLoggetInn = "";
+
+        private readonly Mock<IBrukerRepository> mockRep = new Mock<IBrukerRepository>();
+        private readonly Mock<ILogger<BrukerController>> mockLog = new Mock<ILogger<BrukerController>>();
+
+        private readonly Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+        private readonly MockHttpSession mockSession = new MockHttpSession();
+
+        [Fact]
+        public async Task HentAlleOk()
+        {
+            var brukerController = new BrukerController(mockRep.Object, mockLog.Object);
+        }
+    }
+}
