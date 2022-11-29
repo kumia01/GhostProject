8d3cda189549d34978ff60bf05b9bbaf0f5f6a2b
ErsanSinani <ErsanSinani@users.noreply.github.com>
2022-11-29 16:37:55 +0100

    fiksa litt bugs

--- GhostProjectv2/Models/Transaksjon.cs
+++ GhostProjectv2/Models/Transaksjon.cs
@@ -5,15 +5,10 @@ namespace GhostProjectv2.Models
     public class Transaksjon
     {
         public int Id { get; set; }
-        //[RegularExpression(@"[0-9]{1,20}")]
         public int Volum { get; set; }
-        //[RegularExpression(@"^[0-9]([\.\,][0-9]{1,10})?")]
         public double Pris { get; set; }
-        //[RegularExpression(@"[0,9]{1,20}")]
         public int BrukereId { get; set; }
-        //[RegularExpression(@"[0,9]{1,20}")]
         public int FlereAksjerId { get; set; }
-        [RegularExpression(@"[0-9a-zA-Z. \-]{2,35}$")]
         public string Ticker { get; set; }
     }
 }
